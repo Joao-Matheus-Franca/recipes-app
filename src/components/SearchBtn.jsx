fc1f9832caecabcb19eb7f5238cb2de304e2a42c
João Matheus Silva Franca <matheussfranca@live.com>
2022-10-24 19:34:30 -0300

    Concluído: Requisito 09

--- src/components/SearchBtn.jsx
+++ src/components/SearchBtn.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import image from '../images/searchIcon.svg';
 
 export default function SearchBtn() {
+  const [state, setState] = useState(false);
+
   return (
-    <button
-      value="Search"
-      type="button"
-      data-testid="search-top-btn"
-      src="src/images/searchIcon.svg"
-      alt="search-top"
-    />
+    <>
+      <button
+        value="Search"
+        type="button"
+        data-testid="search-top-btn"
+        src={ image }
+        onClick={ () => setState(!state) }
+      >
+        <img src={ image } alt="search-top" />
+      </button>
+      { state && (<input data-testid="search-input" type="text" />) }
+    </>
   );
 }
