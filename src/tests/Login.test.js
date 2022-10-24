8dea3cf49b4ddf47184c8c88ee0dc7e075a1eabe
João Matheus Silva Franca <matheussfranca@live.com>
2022-10-24 17:33:16 -0300

    Concluído: Requisito 06

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('Testes da página de Login', () => {
   test('Elementos de Login', () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    );
+    // const { history } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     const title = screen.getByRole('heading', { name: /login/i });
     const emailInput = screen.getByTestId('email-input');
     const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByTestId('login-submit-btn');
     expect(title).toBeInTheDocument();
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+
+    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(passwordInput, '123456789');
+
+    expect(button).not.toBeDisabled();
+
+    userEvent.click(button);
+
+    // const { pathName } = history.location;
+    // expect(pathName).toBe('/meals');
   });
 });
