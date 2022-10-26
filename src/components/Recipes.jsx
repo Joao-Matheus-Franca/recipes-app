44adf61189f8b923303843f344da898b01b5a838
João Matheus Silva Franca <matheussfranca@live.com>
2022-10-26 16:29:54 -0300

    Concluído: Requisito 20

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -6,27 +6,42 @@ function Recipes() {
 
   const [state, setState] = useState([]);
 
+  const [categories, setCategories] = useState([]);
+
   const fetchRecipes = async (page) => {
     if (page === 'Meals') {
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const respCategories = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
       const data = await response.json();
+      const dataCategories = await respCategories.json();
       setState(data.meals);
-      return data;
+      setCategories(dataCategories.meals);
     } if (page === 'Drinks') {
       const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const respCategories = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
       const data = await response.json();
+      const dataCategories = await respCategories.json();
       setState(data.drinks);
-      return data;
+      setCategories(dataCategories.drinks);
     }
   };
 
   useEffect(() => { fetchRecipes(local); }, [local]);
 
   const maxNumber = 12;
+  const maxCategories = 5;
 
   return (
     <>
-      <div />
+      { categories.filter((_, i) => i < maxCategories).map((c) => (
+        <button
+          key={ c.strCategory }
+          type="button"
+          data-testid={ `${c.strCategory}-category-filter` }
+        >
+          { c.strCategory }
+        </button>
+      )) }
       { local === 'Meals' ? state.filter((_, i) => i < maxNumber).map((r, i) => (
         <div key={ r.idMeal } data-testid={ `${i}-recipe-card` }>
           <h3 data-testid={ `${i}-card-name` }>{r.strMeal}</h3>
---
+++ src/tests/Recipes.test.js
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react';
+import Recipes from '../components/Recipes';
+import renderWithContext from './helpers/renderWithContext';
+
+describe('Testes do componente Recipes', () => {
+  const contextMeals = {
+    local: 'Meals',
+  };
+  const mockMeals = {
+    meals: [{
+      idMeal: 1,
+      strMeal: 'Hambúrguer',
+      strMealThumb: 'https://i1.wp.com/mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food.jpg',
+      strCategory: 'Fast Food',
+    }],
+  };
+
+  const contextDrinks = {
+    local: 'Drinks',
+  };
+  const mockDrinks = {
+    drinks: [{
+      idMeal: 1,
+      strMeal: 'MilkShake',
+      strMealThumb: 'https://img.freepik.com/fotos-premium/tres-copos-de-milkshake-de-sabores-variados-milkshake-de-chocolate-baunilha-e-morango_434193-2702.jpg?w=2000',
+      strCategory: 'Fast Food',
+    }],
+  };
+
+  test('Teste de renderizaçãos dos elementos na página Meals', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(mockMeals),
+    }));
+
+    renderWithContext(<Recipes />, contextMeals);
+
+    const categoryButton = await screen.findByText('Fast Food');
+    expect(categoryButton).toBeInTheDocument();
+  });
+
+  test('Teste de renderizaçãos dos elementos na página Drinks', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(mockDrinks),
+    }));
+
+    renderWithContext(<Recipes />, contextDrinks);
+
+    const categoryButton = await screen.findByText('Fast Food');
+    expect(categoryButton).toBeInTheDocument();
+  });
+});
