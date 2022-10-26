bef26b9d8e086d6642209f2bfefd5166e39cb4ab
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-10-25 21:55:56 -0300

    grupo 28 - requisito 16

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import ProfileBtn from '../components/ProfileBtn';
 import SearchBtn from '../components/SearchBtn';
 import Context from '../context.js/Context';
+import Footer from '../components/Footer';
 
 export default function Meals(props) {
   const { setLocal, dataSearch } = useContext(Context);
@@ -27,6 +28,7 @@ export default function Meals(props) {
         <h1 data-testid="page-title"> Meals </h1>
         <ProfileBtn />
         <SearchBtn />
+        <Footer />
       </Header>
     </div>
   );
