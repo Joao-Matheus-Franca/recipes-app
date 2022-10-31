4833a1d1acff54c56683dacb4756f57f64e3677d
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-10-31 17:42:27 -0300

    requisito 60, 61, 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import ProfileBtn from '../components/ProfileBtn';
@@ -13,25 +14,39 @@ export default function Profile() {
         <ProfileBtn />
       </Header>
       <main>
-        {userEmail && <p data-testid="profile-email">{ userEmail.email }</p>}
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Done Recipes
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </button>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-        >
-          Logout
-        </button>
+        <p data-testid="profile-email">{ userEmail.email }</p>
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
+
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => {
+              localStorage.removeItem('user');
+              localStorage.removeItem('doneRecipes');
+              localStorage.removeItem('favoriteRecipes');
+              localStorage.removeItem('inProgressRecipes');
+            } }
+          >
+            Logout
+          </button>
+        </Link>
       </main>
       <Footer />
     </div>
--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import Profile from '../pages/Profile';
 
 describe('Testando se a página de Meals', () => {
+  const email = 'michel.ferreira.melo@gmail.com';
   it('Tem o título correto', () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
     renderWithRouter(<Profile />);
 
     const title = screen.getByRole('heading', { name: /profile/i });
@@ -13,9 +15,35 @@ describe('Testando se a página de Meals', () => {
   });
 
   it('Tem um botão', () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
     renderWithRouter(<Profile />);
 
     const btnTopProfile = screen.getByTestId('profile-top-btn');
     expect(btnTopProfile).toBeInTheDocument();
+  });
+
+  it('Tem três botões de redirecionamento', () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    renderWithRouter(<Profile />);
+
+    const btnProfileDone = screen.getByTestId('profile-done-btn');
+    expect(btnProfileDone).toBeInTheDocument();
+
+    const btnProfileFavorite = screen.getByTestId('profile-favorite-btn');
+    expect(btnProfileFavorite).toBeInTheDocument();
+
+    const btnProfileLogout = screen.getByTestId('profile-logout-btn');
+    expect(btnProfileLogout).toBeInTheDocument();
+  });
+
+  it('Tem três botões de redirecionamento', () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    const { history } = renderWithRouter(<Profile />);
+
+    const btnProfileLogout = screen.getByTestId('profile-logout-btn');
+    userEvent.click(btnProfileLogout);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
 });
